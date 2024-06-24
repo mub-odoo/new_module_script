8b69a1b3b3d66a5236ecee8ce3ae0185a9f8ac79
Munaf Khan <munafkhan732@gmail.com>
2024-06-24 12:55:46 +0530

    update readme file

--- script.py
+++ script.py
@@ -6,51 +6,51 @@ import content
 
 
 def create_module_structure(module_name, module_version, file_contents):
-    module_dir = module_name.lower().replace(' ', '_')
+    ModuleDir = module_name.lower().replace(' ', '_')
     
     # Create module directory
-    os.makedirs(module_dir)
+    os.makedirs(ModuleDir)
 
     # Create __init__.py file
-    with open(os.path.join(module_dir, '__init__.py'), 'w') as main_init:
-        main_init.write(file_contents.get('init_content'))
+    with open(os.path.join(ModuleDir, '__init__.py'), 'w') as MainInit:
+        MainInit.write(file_contents.get('init_content'))
 
     # Create .gitignore file
-    with open(os.path.join(module_dir, '.gitignore'), 'w') as GitIgnore:
+    with open(os.path.join(ModuleDir, '.gitignore'), 'w') as GitIgnore:
         GitIgnore.write(file_contents.get('gitignore_content'))
 
     # Create models directory
-    models_dir = os.path.join(module_dir, 'models')
-    os.makedirs(models_dir)
-    with open(os.path.join(models_dir, '__init__.py'), 'w') as init_file:
+    ModelsDir = os.path.join(ModuleDir, 'models')
+    os.makedirs(ModelsDir)
+    with open(os.path.join(ModelsDir, '__init__.py'), 'w') as init_file:
         init_file.write(file_contents.get('init_content'))
         # pass
 
     # Create views directory
-    views_dir = os.path.join(module_dir, 'views')
-    os.makedirs(views_dir)
-    # with open(os.path.join(views_dir, '__init__.py'), 'w'):
+    ViewsDir = os.path.join(ModuleDir, 'views')
+    os.makedirs(ViewsDir)
+    # with open(os.path.join(ViewsDir, '__init__.py'), 'w'):
     #     pass
 
     # Create security directory
-    security_dir = os.path.join(module_dir, 'security')
-    os.makedirs(security_dir)
-    with open(os.path.join(security_dir, 'ir.model.access.csv'), 'w'):
+    SecurityDir = os.path.join(ModuleDir, 'security')
+    os.makedirs(SecurityDir)
+    with open(os.path.join(SecurityDir, 'ir.model.access.csv'), 'w'):
         pass
 
     # Create data directory
-    data_dir = os.path.join(module_dir, 'data')
-    os.makedirs(data_dir)
+    DataDir = os.path.join(ModuleDir, 'data')
+    os.makedirs(DataDir)
 
     # Create static directory
-    static_dir = os.path.join(module_dir, 'static')
-    os.makedirs(static_dir)
+    StaticDir = os.path.join(ModuleDir, 'static')
+    os.makedirs(StaticDir)
 
     # Create manifest file
-    manifest_content = file_contents.get('manifest_content') % (module_name, module_version)
+    ManifestContent = file_contents.get('manifest_content') % (module_name, module_version)
 
-    with open(os.path.join(module_dir, '__manifest__.py'), 'w') as manifest_file:
-        manifest_file.write(manifest_content)
+    with open(os.path.join(ModuleDir, '__manifest__.py'), 'w') as manifest_file:
+        manifest_file.write(ManifestContent)
 
 if __name__ == '__main__':
     module_name = input('Enter the Module Technical Name: ')
